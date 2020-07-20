9ee148fb6026edd90350c36b5e813bcaa94de6f0
Revannare <revannare@outlook.com>
2020-07-21 01:31:39 +0900

    First Version

---
+++ square_rectangle_dp.java
@@ -0,0 +1,293 @@
+import com.google.common.collect.Collections2;
+import com.google.common.primitives.Ints;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ * Package: PACKAGE_NAME
+ * Comments:
+ * Author: lwx
+ * Create Date: 2020/6/27
+ * Modified Date: 2020/6/27
+ * Why & What is modified:
+ * Version: 0.0.1beta
+ * It's the only NEET thing to do. – Shionji Yuuko
+ */
+public class square_rectangle_dp {
+    static HashMap<int[], Integer> results = new HashMap<>();
+    static ConcurrentLinkedQueue<int[]> tempBuffer = new ConcurrentLinkedQueue<>();
+
+    static boolean dpTable[][];
+
+    public static boolean flow_check(int strip[], int lenth, int width) {
+        ArrayList<Integer> tempList = new ArrayList<>();
+        for (int i = 0; i < strip.length; i++) {
+            if (strip[i] == 1){
+                tempList.add(i);
+            }
+        }
+
+        int[] numbers = new int[tempList.size()];
+        for (int i = 0; i < tempList.size(); i++) {
+            numbers[i] = tempList.get(i);
+        }
+
+        List<Integer> vals = Ints.asList(numbers);
+        Collection<List<Integer>> orderPerm = Collections2.permutations(vals);
+
+        for (List<Integer> val : orderPerm) {
+            Integer[] order = new Integer[val.size()];
+            val.toArray(order);
+            if (flow_check_single_step(order, lenth, width)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // This function is to check by flow for one order
+    public static boolean flow_check_single_step(Integer order[], int lenth, int width) {
+        int[] flow = new int[lenth+1];
+        flow[0] = width;
+        for (int i = 1; i < flow.length; i++) {
+            flow[i] = 0;
+        }
+        for (int i = 0; i < order.length; i++) {
+            int targetIndex = 0;
+            for (int k = 0; k < flow.length; k++)
+            {
+                if (flow[k] > 0 && flow[targetIndex] > flow[k]) {
+                    targetIndex = k;
+                }
+            }
+            int moveIndex = order[i] + 1 + targetIndex;
+
+            if (moveIndex > lenth) {
+                return false;
+            } else {
+                flow[targetIndex] = flow[targetIndex] - order[i] - 1;
+                flow[moveIndex] = flow[moveIndex] + order[i] + 1;
+//                if (flow[targetIndex]<0){
+//                    return false;
+//                }
+            }
+        }
+        if (flow[lenth] != width)
+        {
+            return false;
+        }
+        for (int k = 0; k < lenth - 1; k++)
+        {
+            if (flow[k] != 0) {
+                return false;
+            }
+        }
+        for (int i = 0; i < order.length; i++) {
+            System.out.print(order[i] + 1 + " ");
+        }
+        return true;
+    }
+
+
+    static void checkdp(ArrayList<Integer> p, int lenth, int width){
+        int[] strip = new int[width];
+        for (int i = 0; i < strip.length; i++) {
+            strip[i] = 0;
+        }
+        for (int i = 0; i < p.size(); i++) {
+            strip[(int) Math.sqrt(p.get(i)) - 1] = 1;
+        }
+        int count = 0;
+        int max_1 = 0;
+        int max_2 = 0;
+        for (int i = strip.length - 1; i >= 0; i--) {
+            if (strip[i] != 0 && count!=2) {
+                count++;
+                max_1 = max_1==0?i:max_1;
+                max_2 = max_1==0?0:i;
+            }
+        }
+        if (max_1+max_2+2 > lenth){
+            return;
+        }
+        flow_check(strip, lenth, width);
+    }
+
+    public static void printSubsetsRec(int arr[], int i, int sum,
+                                ArrayList<Integer> p, int lenth, int width)
+    {
+        // If we reached end and sum is non-zero. We print
+        // p[] only if arr[0] is equal to sun OR dp[0][sum]
+        // is true.
+        if (i == 0 && sum != 0 && dpTable[0][sum])
+        {
+            p.add(arr[i]);
+            checkdp(p, lenth, width);
+            p.clear();
+            return;
+        }
+
+        // If sum becomes 0
+        if (i == 0 && sum == 0)
+        {
+            checkdp(p, lenth, width);
+            p.clear();
+            return;
+        }
+
+        // If given sum can be achieved after ignoring
+        // current element.
+        if (dpTable[i-1][sum])
+        {
+            // Create a new vector to store path
+            ArrayList<Integer> b = new ArrayList<>();
+            b.addAll(p);
+            printSubsetsRec(arr, i-1, sum, b, lenth, width);
+        }
+
+        // If given sum can be achieved after considering
+        // current element.
+        if (sum >= arr[i] && dpTable[i-1][sum-arr[i]])
+        {
+            p.add(arr[i]);
+            printSubsetsRec(arr, i-1, sum-arr[i], p, lenth, width);
+        }
+    }
+
+    public static void printAllSubsets(int arr[], int n, int sum, int lenth, int width)
+    {
+        if (n == 0 || sum < 0)
+            return;
+
+        // Sum 0 can always be achieved with 0 elements
+        dpTable = new boolean[n][sum + 1];
+        for (int i=0; i<n; ++i)
+        {
+            dpTable[i][0] = true;
+        }
+
+        // Sum arr[0] can be achieved with single element
+        if (arr[0] <= sum)
+            dpTable[0][arr[0]] = true;
+
+        // Fill rest of the entries in dp[][]
+        for (int i = 1; i < n; ++i)
+            for (int j = 0; j < sum + 1; ++j)
+                dpTable[i][j] = (arr[i] <= j) ? (dpTable[i-1][j] ||
+                        dpTable[i-1][j-arr[i]])
+                        : dpTable[i - 1][j];
+        if (dpTable[n-1][sum] == false)
+        {
+            System.out.println("There are no subsets with" +
+                    " sum "+ sum);
+            return;
+        }
+
+        // Now recursively traverse dp[][] to find all
+        // paths from dp[n-1][sum]
+        ArrayList<Integer> p = new ArrayList<>();
+        printSubsetsRec(arr, n-1, sum, p, lenth, width);
+    }
+
+    // main
+    public static void main(String[] args) {
+
+//        int p = 33;
+//        int kk = 32;
+////        int num[] = {1,4,7,8,9,10,14,15,18};
+//        int num[] = {0,3,6,7,8,9,13,14,17};
+//        int arrayN[] = new int[kk];
+//        for (int i = 0; i < kk; i++) {
+//            arrayN[i] = 0;
+//        }
+//        for (int i = 0; i < num.length; i++) {
+//            arrayN[num[i]] = 1;
+//        }
+//        System.out.println(flow_check(arrayN, 33, 32));
+
+        // define the length and input from users
+        int lenth = 0;
+        int width = 0;
+        Scanner input=new Scanner(System.in);
+        System.out.println("input the length of the rectangle:");
+        lenth=input.nextInt();
+        System.out.println("input the width of the rectangle:");
+        width=input.nextInt();
+
+        int temp_w = Math.min(lenth, width);
+        int temp_l = Math.max(lenth, width);
+        width = temp_w;
+        lenth = temp_l;
+
+        System.out.println("start checking the length of the square: "+lenth+" x "+width);
+
+
+        int arr[] = new int[width];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (i+1) * (i+1);
+        }
+        int n = arr.length;
+        int sum = width * lenth;
+
+        printAllSubsets(arr, n, sum, lenth, width);
+
+//        int endLabel = 0;
+//        int temp = 0;
+//        int target = width*lenth;
+//        while (temp <= target) {
+//            temp = temp + (endLabel + 1) * (endLabel + 1);
+//            endLabel++;
+//        }
+//
+//        for (int i = width - 2; i >= endLabel; i--)
+//        {
+//            int[] tempMask = new int[width];
+//            for (int k = 0; k < width; k++)
+//            {
+//                tempMask[k] = 0;
+//            }
+//            tempMask[i] = 1;
+//
+//            int endIndex = lenth - i - 1;
+//
+//            while (tempMask[endIndex] != 1) {
+//                tempMask[0] ++;
+//                for (int j = 0; j < i; j++) {
+//                    if (tempMask[j] == 2){
+//                        tempMask[j] = 0;
+//                        tempMask[j+1]++;
+//                    }
+////                    else if (tempMask[j] == 1 || tempMask[j] == 0) {
+////                        break;
+////                    }
+//                }
+//                //check
+//                int count = 0;
+//                for (int k = 0; k < width; k++)
+//                {
+//                    if(tempMask[k] == 1)
+//                        count += (k+1)*(k+1);
+//                }
+//                int tempResult = count - target;
+//                if (tempResult==0){
+//                    if(flow_check(tempMask, lenth, width)){
+//                        return;
+//                }
+////                    results.put(tempMask.clone(), 1);
+//                }
+//                else if(tempResult>0){
+//                    break;
+//                }
+//            }
+//        }
+////        System.out.println(results.size());
+////
+////        Set<int[]> masks = results.keySet();
+////        for (int[] mask : masks) {
+////            if(flow_check(mask, lenth, width)){
+////                return;
+////            }
+////        }
+    }
+}
